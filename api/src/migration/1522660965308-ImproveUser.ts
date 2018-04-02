332f8bc45f19a57ce6e01586226656891d221552
Can Ka <kattwinkel@w11k.de>
2018-04-02 11:24:15 +0200

    feat(auth): gives users passwords and a user level

---
+++ api/src/migration/1522660965308-ImproveUser.ts
@@ -0,0 +1,15 @@
+import {MigrationInterface, QueryRunner} from 'typeorm';
+
+export class ImproveUser1522660965308 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.query(`ALTER TABLE "public"."user_entity" ADD "password" character varying(60) NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "public"."user_entity" ADD "userLevel" integer NOT NULL DEFAULT 1`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.query(`ALTER TABLE "public"."user_entity" DROP "userLevel"`);
+        await queryRunner.query(`ALTER TABLE "public"."user_entity" DROP "password"`);
+    }
+
+}
--- api/src/modules/user/user.entity.ts
+++ api/src/modules/user/user.entity.ts
@@ -1,10 +1,19 @@
-import {Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn} from 'typeorm';
+import {
+    BeforeInsert,
+    BeforeUpdate,
+    Column,
+    CreateDateColumn,
+    Entity,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn
+} from 'typeorm';
+import {UserLevel} from './user-level.enum';
+import {hashIt} from '../../utils/encrypt';
 
 @Entity()
 export class UserEntity {
     @PrimaryGeneratedColumn()
     id: number;
-
 
     @Column()
     mail: string;
@@ -15,5 +24,23 @@ export class UserEntity {
     @UpdateDateColumn({type: 'timestamp'})
     updatedAt: Date;
 
+    @Column({select: false, length: 60})
+    password?: string;
+
+    @Column({type: 'integer', default: UserLevel.PrivateUser})
+    userLevel: UserLevel;
+
+    // I'm not sure yet whether this logic would not be
+    // better kept in the user service. In any case,
+    // the trigger is not called when a pure object
+    // is inserted. Instead, an instance must be created
+    // manually and then saved.
+    @BeforeInsert()
+    @BeforeUpdate()
+    async hashPassword() {
+        if (this.password) {
+            this.password = await hashIt(this.password);
+        }
+    }
 
 }
